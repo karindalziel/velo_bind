b62e0c08ba8b301f6c6073068114c59c6b34c38c
Scott Kobewka <kobewka@gmail.com>
2016-05-05 11:46:10 -0500

    proper-ish resonse

--- app/models/dpl_api.rb
+++ app/models/dpl_api.rb
@@ -6,7 +6,9 @@ class DplApi
   base_uri 'api.dp.la/v2'
 
   def search(string)
-    self.class.get("/items", {q: string, api_key: 'b8f3057f7527a5248ff65e41ff9e9e7f'})
+    self.class.get("/items?q=#{string}&api_key=b8f3057f7527a5248ff65e41ff9e9e7f")
   end
+
+
 
 end
