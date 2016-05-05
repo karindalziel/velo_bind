5b9cd2355227390d5f890c370b0b3da6c712b952
Scott Kobewka <kobewka@gmail.com>
2016-05-05 11:42:24 -0500

    Unauthorized

---
+++ app/models/dpl_api.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+class DplApi
+  include HTTParty
+
+  base_uri 'api.dp.la/v2'
+
+  def search(string)
+    self.class.get("/items", {q: string, api_key: 'b8f3057f7527a5248ff65e41ff9e9e7f'})
+  end
+
+end
